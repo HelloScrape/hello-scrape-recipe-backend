f8d469f93a02adeb286fdce271bbffd6f3875e0d
Jaap <jaap@mrproper.dev>
2023-01-18 12:49:02 +0100

    Update recipeRouter.ts

--- src/routes/recipeRouter.ts
+++ src/routes/recipeRouter.ts
@@ -1,11 +1,11 @@
 import express, { Response, Request } from 'express';
-import recipe from '../models/recipe';
+import recipe, { recipeSchemaType } from '../models/recipe';
 import log from 'fancy-log';
 
 const router = express.Router();
 
 router.get('/', async function (req: Request, res: Response) {
-  const recipes = await recipe.find({});
+  const recipes = fixRecipeImageUrls(await recipe.find({}));
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
   res.json({ data: recipes, error: null, imageUrl: process.env.PUBLIC_IMAGES_PATH });
 });
@@ -19,15 +19,27 @@ router.get('/language/:language', async function (req: Request, res: Response) {
     res.status(400).json({ error: 'You must provide a valid language (en,nl,fr)!', data: null });
     return;
   }
-  const recipes = await recipe.find({ language: req.params.language });
+  const recipes = fixRecipeImageUrls(await recipe.find({ language: req.params.language }));
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
   res.json({ data: recipes, error: null, imageUrl: process.env.PUBLIC_IMAGES_PATH });
 });
 
 router.post('/id/:id', async function (req: Request, res: Response) {
-  const recipes = await recipe.findOne({ _id: req.params.id });
+  const recipes = fixRecipeImageUrls(await recipe.find({ _id: req.params.id }));
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
   res.json({ data: recipes, error: null, imageUrl: process.env.PUBLIC_IMAGES_PATH });
 });
 
 export default router;
+
+
+function fixRecipeImageUrls(recipes: recipeSchemaType[]) {
+  recipes.forEach((recipeItem) => {
+    recipeItem.images = recipeItem.images.map((image) => {
+      // remove \n before and after the image name
+      image = image.replace(/(\r\n|\n|\r)/gm, '').trim();
+      return `${process.env.PUBLIC_IMAGES_PATH}/${image}`;
+    });
+  });
+  return recipes;
+}
